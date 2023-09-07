899886b5fc2efbd5de19c3e2f5c2b198a7ad27df
carce2023 <arcegadeacecilia@gmail.com>
2023-09-07 02:51:26 +0000

    Hiperparametros oroginales

--- src/lightgbm/424_lightgbm_final_1.r
+++ src/lightgbm/424_lightgbm_final_1.r
@@ -134,7 +134,7 @@ setorder(tb_entrega, -prob)
 # suba TODOS los archivos a Kaggle
 # espera a la siguiente clase sincronica en donde el tema sera explicado
 
-cortes <- seq(1000, 16000, by = 500)
+cortes <- seq(1000, 2500, by = 500)
 for (envios in cortes) {
   tb_entrega[, Predicted := 0L]
   tb_entrega[1:envios, Predicted := 1L]
