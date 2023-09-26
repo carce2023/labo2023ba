9b4d0756ef2f9ce0ec4149e5954a0cac5eb0eae7
carce2023 <arcegadeacecilia@gmail.com>
2023-09-26 02:02:39 +0000

    012

--- src/workflow-inicial/601_RUN_correr_workflow_012.r
+++ src/workflow-inicial/601_RUN_correr_workflow_012.r
@@ -10,11 +10,11 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/labo2023ba/src/workflow-inicial/611_CA_reparar_dataset_007.r")
-source("~/labo2023ba/src/workflow-inicial/621_DR_corregir_drifting_007.r")
-source("~/labo2023ba/src/workflow-inicial/631_FE_historia_014.r")
-source("~/labo2023ba/src/workflow-inicial/641_TS_training_strategy_014.r")
+source("~/labo2023ba/src/workflow-inicial/611_CA_reparar_dataset_000.r")
+source("~/labo2023ba/src/workflow-inicial/621_DR_corregir_drifting_000.r")
+source("~/labo2023ba/src/workflow-inicial/631_FE_historia_012.r")
+source("~/labo2023ba/src/workflow-inicial/641_TS_training_strategy_012.r")
 
 # ultimos pasos, muy lentos
-source("~/labo2023ba/src/workflow-inicial/651_HT_lightgbm_014.r")
-source("~/labo2023ba/src/workflow-inicial/661_ZZ_final_014.r")
+source("~/labo2023ba/src/workflow-inicial/651_HT_lightgbm_012.r")
+source("~/labo2023ba/src/workflow-inicial/661_ZZ_final_012.r")
