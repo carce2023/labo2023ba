8bdfd51721e1496b0f76d8f3adf55028d768d917
carce2023 <arcegadeacecilia@gmail.com>
2023-10-04 21:51:06 +0000

    ...

---
+++ src/workflow-inicial/601_RUN_correr_workflow_f001_10semillas.r
@@ -0,0 +1,13 @@
+# Corrida general del workflow
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+
+# corrida de cada paso del workflow
+
+# primeros pasos, relativamente rapidos
+source("~/labo2023ba/src/workflow-inicial/661_ZZ_final_006f_10semillas.r")
